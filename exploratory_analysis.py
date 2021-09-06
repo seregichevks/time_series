41c2640e2d3bc5aaa70bc81b01a3cfa1959eb4d7
seregichevks <seregichevks@yandex.ru>
2021-09-06 08:29:19 +0300

    added any other from HW_workshop_2.ipynb to exploratory_analysis.py

--- exploratory_analysis.py
+++ exploratory_analysis.py
@@ -6,6 +6,8 @@ import missingno as msno
 from datetime import datetime, timedelta
 
 import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 import seaborn as sns
 
 plt.style.use('seaborn-darkgrid')
@@ -18,3 +20,33 @@ df = pd.read_csv('sensors.csv')
 
 # let's look at the data
 df
+
+# let's look at the presence of missing values
+msno.bar(df)
+msno.matrix(df)
+
+# we will output statistics for each column
+df.dtypes
+df.describe(include=[np.number]) # number columns
+df.describe(include=[np.object]) # not number
+
+# Nan values -> median
+for col in df.columns[1:]:
+  df[col].fillna(df[col].median(), inplace=True)
+
+# pairwise correlation
+correlation = df.corr()
+plt.figure(figsize=(35,35))
+sns.heatmap(correlation, vmax=1, square=True, annot=True, cmap='cubehelix')
+
+plt.title('Correlation between different features')
+plt.show()
+
+# all graf
+fig = go.Figure()
+for col in df.columns[1:]:
+  fig.add_trace(go.Scatter(x=data['timestamp'], y=data[col],
+                    mode='lines',
+                    name=col))
+
+fig.show()
